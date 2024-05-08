318fc28ee3b0b784264e72784f476e455a984d61
connor-mcbride <cmcbride1024@gmail.com>
2024-05-08 14:23:54 -0600

    Added startup screen and favicon

--- django-app/influence_maximization/influence_game/static/js/game.js
+++ django-app/influence_maximization/influence_game/static/js/game.js
@@ -1,3 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Game.js loaded!");
-});
+function startGame() {
+    const graphType = document.getElementById('graph-type').value;
+    const player1 = document.getElementById('player1').value;
+    const player2 = document.getElementById('player2').value;
+
+    document.getElementById('startup-screen').style.display = 'none';
+
+    console.log('Starting game with:', { graphType, player1, player2 });
+}
